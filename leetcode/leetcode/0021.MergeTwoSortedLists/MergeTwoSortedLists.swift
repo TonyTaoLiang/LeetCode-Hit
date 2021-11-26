04d06b8f76c97cc9876cdbba1e864651e4cd72db
Tonytaoliang <tony_taoliang@163.com>
2021-11-26 11:19:47 +0800

    1.合并2条链表-递归解法

--- leetcode/leetcode/0021.MergeTwoSortedLists/MergeTwoSortedLists.swift
+++ leetcode/leetcode/0021.MergeTwoSortedLists/MergeTwoSortedLists.swift
@@ -9,7 +9,7 @@ import Foundation
 
 
 class MergeTwoSortedLists{
-    //快排的方式13.62% 9.36%
+    //类似归并排序的方式
     func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
 
         var res = ListNode()
@@ -37,5 +37,25 @@ class MergeTwoSortedLists{
         return head.next
     }
 
+    //递归
+    func mergeTwoLists2(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+
+        if l1 == nil {
+            return l2
+        }
+
+        if l2 == nil {
+            return l1
+        }
+
+        if l1!.val < l2!.val {
+            l1?.next = mergeTwoLists2(l1?.next, l2)
+            return l1
+        }
+
+
+        l2?.next = mergeTwoLists2(l1, l2?.next)
+        return l2
+    }
     
 }
