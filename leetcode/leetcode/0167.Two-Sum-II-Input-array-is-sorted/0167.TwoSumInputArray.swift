bf4332466ac79166875900e9a5604a91f258f27c
Tonytaoliang <tony_taoliang@163.com>
2022-02-12 12:18:36 +0800

    1.167两数之和 II - 输入有序数组补充一个二分法

--- leetcode/leetcode/0167.Two-Sum-II-Input-array-is-sorted/0167.TwoSumInputArray.swift
+++ leetcode/leetcode/0167.Two-Sum-II-Input-array-is-sorted/0167.TwoSumInputArray.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class TwoSumInputArray{
 
+    //解法一：双指针
     func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
 
         var i: Int = 1, j: Int = numbers.count
@@ -31,4 +32,32 @@ class TwoSumInputArray{
         return []
 
     }
+
+    //解法二：二分，先固定一个元素，然后二分查找另外一个
+    func twoSum2(_ numbers: [Int], _ target: Int) -> [Int] {
+
+        for i in 0..<numbers.count {
+
+            var low = i + 1
+            var high = numbers.count - 1
+
+            while low <= high {
+
+                let middle = (high - low)/2 + low
+
+                if numbers[middle] == target - numbers[i] {
+                    return [i+1,middle+1]
+                } else if numbers[middle] > target - numbers[i]{
+                    high = middle - 1
+                } else {
+                    low = middle + 1
+                }
+
+            }
+
+        }
+
+        return []
+    }
+
 }
