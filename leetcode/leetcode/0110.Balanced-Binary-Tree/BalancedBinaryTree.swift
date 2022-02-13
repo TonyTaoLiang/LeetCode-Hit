5564fc7146eda2d226a2dfc2db362d7c658d1452
Tonytaoliang <tony_taoliang@163.com>
2022-02-13 23:48:16 +0800

    1.判断是否是平衡二叉树。104先计算节点高度再递归左右子树

---
+++ leetcode/leetcode/0110.Balanced-Binary-Tree/BalancedBinaryTree.swift
@@ -0,0 +1,35 @@
+//
+//  BalancedBinaryTree.swift
+//  leetcode
+//
+//  Created by Tonytaoliang on 2022/2/13.
+//
+
+import Foundation
+
+class BalancedBinaryTree {
+
+    func isBalanced(_ root: TreeNode?) -> Bool {
+
+        if root == nil {
+            return true
+        }
+
+        if abs(heightForNode(root?.left) - heightForNode(root?.right)) > 1{
+            return false
+        }
+
+        //依次递归，左右子树都得是平衡二叉树
+        return isBalanced(root?.left) && isBalanced(root?.right)
+    }
+
+    //104题计算树节点的高度
+    func heightForNode(_ root: TreeNode?) -> Int {
+
+        if root == nil {
+            return 0
+        }
+
+        return max(heightForNode(root?.left), heightForNode(root?.right)) + 1
+    }
+}
