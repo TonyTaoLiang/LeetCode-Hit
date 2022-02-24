df49f6c84cd50e9e93a4f6eb59070c48055dab97
Tonytaoliang <tony_taoliang@163.com>
2022-02-24 11:00:33 +0800

    1.整数反转- 取模（字符串，栈也可吧）

--- leetcode/leetcode/0007.Reverse-Integer/ReverseInteger.swift
+++ leetcode/leetcode/0007.Reverse-Integer/ReverseInteger.swift
@@ -9,9 +9,25 @@ import Foundation
 
 class ReverseInteger {
 
+    //第一反应是转字符串，后面才想到取模
     func reverse(_ x: Int) -> Int {
 
+        var x = x
+        var temp = 0
 
+        while x != 0 {
+
+            temp = temp*10 + x%10
+            x = x/10
+        }
+        //左移，判断int32的范围
+        if temp > 1 << 31 - 1 || temp < -(1 << 31) {
+            return 0
+        }
+
+        return temp
+        
+        //return ret >= Int32.max || ret <= Int32.min ? 0 : ret
 
     }
 
