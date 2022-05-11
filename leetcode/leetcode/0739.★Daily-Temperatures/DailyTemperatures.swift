fe459d660df525798bf9aa9ff07c06610702e2a7
Tonytaoliang <tony_taoliang@163.com>
2022-05-11 16:24:22 +0800

    1.复习第十五天：单调栈 每日天气。 找最近一个比它大的 自然单调栈

--- leetcode/leetcode/0739.★Daily-Temperatures/DailyTemperatures.swift
+++ leetcode/leetcode/0739.★Daily-Temperatures/DailyTemperatures.swift
@@ -38,3 +38,31 @@ class DailyTemperatures {
         return res
     }
 }
+
+class DailyTemperaturess {
+
+    //从后往前遍历 维护一个递减的单调栈
+    func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
+
+        var stack = Stack<Int>()
+        var res = Array(repeating: 0, count: temperatures.count)
+        var i = temperatures.count - 1
+
+        while i >= 0 {
+
+            while !stack.isEmpty && temperatures[i] >= temperatures[stack.peek()!]{
+                stack.pop()
+            }
+
+            if let top = stack.peek() {
+                res[i] = top - i
+            }
+
+            stack.push(i)
+
+            i -= 1
+        }
+
+        return res
+    }
+}
