73ef8c39fbf92c0f7b33fe99fe4e0a414187d172
Tonytaoliang <tony_taoliang@163.com>
2022-06-12 17:02:27 +0800

    1.复习20天，递归合并2个有序链表

--- leetcode/leetcode/0021.MergeTwoSortedLists/MergeTwoSortedLists.swift
+++ leetcode/leetcode/0021.MergeTwoSortedLists/MergeTwoSortedLists.swift
@@ -61,5 +61,25 @@ public class MergeTwoSortedLists{
 //        l2?.next = mergeTwoLists2(l1, l2?.next)
 //        return l2
     }
-    
+
+    func mergeTwoLists3(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+
+        if l1 == nil {
+            return l2
+        }
+
+        if l2 == nil {
+            return l1
+        }
+
+        if l1!.val <= l2!.val {
+            l1?.next = mergeTwoLists3(l1?.next, l2)
+            return l1
+        } else {
+            l2?.next = mergeTwoLists3(l1, l2?.next)
+            return l2
+        }
+
+    }
+
 }
